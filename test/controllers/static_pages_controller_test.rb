cb6426ed36bd6f1c49141203504da256baca3ff8
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-04 23:56:11 +0000

    test: Change `static_pages_index_url` to `static_pages_url`

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get static_pages_index_url
+    get static_pages_url
     assert_response :success
   end
 end
